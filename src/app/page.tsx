5ade9e9580037029a258afb8fe72042c24602efc
Dhanraj_Kamble <dhanrajkamble3518@gmail.com>
2025-02-11 12:09:55 +0530

    wait to complete next js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+'use client'
+import EventsFunctionsState from "@/components/EventsFunctionsState";
 
 export default function Home() {
   return (
      <div>
       <h1>Hello World...!</h1>
+      <EventsFunctionsState />
     </div>
   );
 }
---
+++ src/components/EventsFunctionsState.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const EventsFunctionsState = () => {
+    // const sayhello = (name: string) => {
+    //     alert(`Hello ${name}`);
+    // }
+
+    // useEffect(()=>{
+    //     sayhello("Dhanraj Kmable");
+    // },[])
+
+    return (
+        <>
+            <h2>Events Functions & State I will complete 40 videos today....!!!</h2>
+            {/* <button onClick={()=>alert("Hello Next App")}>Click Me</button> */}
+            {/* <button onClick={()=>sayhello("Dhanraj Kamble")}>Function Call in Button</button> */}
+
+            <button>Function Call in Button</button>
+
+        </>
+    );
+}
+
+export default EventsFunctionsState;
